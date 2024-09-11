427842f67c091115a58963a20a409897e64d0ca8
AdaliCode <fiezayyan@gmail.com>
2024-09-11 14:42:22 +0700

    membuat table awal movies

---
+++ database/seeders/MovieSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Movie;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class MovieSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $movie = new Movie();
+        $movie->title = "One Piece GOLD";
+        $movie->slug = Str::slug($movie->title);
+        $movie->release = "2024/09/11";
+        $movie->save();
+    }
+}
